1ea86000bc81a819d82b6e59ebf5e6cb4152659c
Angel Solano <143917687+DSVAngel@users.noreply.github.com>
2025-04-24 19:03:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require ('dotenv').config()
 
 const server = require ('./server');
-const PORT = process.env.ROOT || 8080;
+const PORT = process.env.ROOT || 8081;
 
 server.listen(PORT, () => console.log('Server is live at localhost:${PORT}'));
